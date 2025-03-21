ca49197721d22eed12757b0e4ee0ba67d626aa77
Ganteda Lakkshmi Prasanna kumaar <chaitanya@Gantedas-Air.bbrouter>
2025-03-21 20:30:37 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,161 @@
+const logotext = "Lakshmi Prasanna Kumar";
+const meta = {
+    title: "Lakshmi Prasanna Kumar",
+    description: "I’m Lakshmi Prasanna Kumar _ Full stack devloper,currently studying in Lendi Institute of Engineering and Technology",
+};
+
+const introdata = {
+    title: "I’m Lakshmi Prasanna Kumar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool full stack websites",
+        third: "I deploy projects in cloud(AWS)",
+    },
+    description: "A passionate CSE student diving into Java full-stack development, DevOps, and cloud technologies while creating innovative projects.",
+    your_img_url: "https://i.ibb.co/tTC58zNn/Whats-App-Image-2025-03-09-at-19-38-39.jpg",
+};
+
+const dataabout = {
+    title: "About MY SELF",
+    aboutme: "I am a final-year Computer Science and Engineering student at Lendi Institute of Engineering and Technology. I have a strong passion for full-stack development and problem-solving. I have completed a Java Full Stack Developer course and gained hands-on experience through internships and hackathons.I enjoy building web applications, designing logos, and working on creative projects. My goal is to keep learning new technologies and apply them to real-world challenges.Feel free to connect with me on LinkedIn or GitHub.",
+};
+const education = [
+    {
+        qualification: "10th Class",
+        institution: "Narayana High School",
+        location: "Vizianagaram",
+        year: "2019-20",
+    },
+    {
+        qualification: "Intermediate",
+        institution: "Narayana Junior College",
+        location: "Visakhapatnam",
+        year: "2020-22",
+    },
+    {
+        qualification: "B.Tech",
+        institution: "Lendi Institute of Engineering and Technology",
+        location: "Vizianagaram",
+        year: "2022-26",
+    },
+];
+
+const technicalSkills = [
+    {
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "HTML & CSS",
+        value: 85,
+    },
+    {
+        name: "JavaScript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "CI/CD tools",
+        value: 60,
+    },
+    {
+        name: "Red Hat Enterprise Linux",
+        value: 65,
+    },
+    {
+        name: "Spring Boot",
+        value: 70,
+    }
+];
+
+const skillCategories = [
+    {
+        title: "Full-Stack Development",
+        description: "Experienced in building web applications using Java, Spring Boot, React, and JavaScript with a strong understanding of both frontend and backend development.",
+    },
+    {
+        title: "DevOps & Cloud",
+        description: "Knowledge of DevOps tools and practices, including CI/CD, Docker, Kubernetes, and cloud platforms, with a focus on automation and deployment.",
+    },
+    {
+        title: "Red Hat Enterprise Linux",
+        description: "Understanding of RHEL system administration, shell scripting, and server management, essential for cloud and enterprise applications.",
+    },
+    {
+        title: "Frontend Development",
+        description: "Skilled in HTML, CSS, Tailwind CSS, and JavaScript, with experience in creating responsive and visually appealing UI components.",
+    },
+    {
+        title: "Backend Development",
+        description: "Proficient in Java, Spring Boot, and RESTful APIs, ensuring secure and efficient server-side application development.",
+    }
+];
+
+
+
+const dataportfolio = [
+    {
+      img: "https://i.ibb.co/FLSLqW8m/payroll.webp",  // Add your image path
+      description: "A software solution that automates salary calculations, tax deductions, and employee payments. It ensures compliance with regulations, accurate financial records, and timely disbursements",
+      link: "https://github.com/Chaiudbbhd/Payroll_System.git", // GitHub link
+    },
+    {
+      img: "https://i.ibb.co/8gX3ZZRx/love.webp",
+      description: "A special day to celebrate love and affection, often observed by exchanging gifts, flowers, and heartfelt messages. It symbolizes deep emotional connections and appreciation for loved ones",
+      link: "https://github.com/yourusername/spotify-clone",
+    },
+    {
+      img: "https://i.ibb.co/vCSCyjV0/language.webp",
+      description: "A real-time language translation system that converts spoken or written text instantly. It enables seamless communication across different languages, breaking barriers in conversations and global interactions",
+      link: "https://github.com/Chaiudbbhd/Live-Language-Translater.git",
+    },
+    {
+      img: "https://i.ibb.co/QSyNWCS/chat.webp",
+      description: "A real-time communication platform enabling users to send messages, share media, and engage in conversations. It supports instant messaging, group chats, and secure data encryption",
+      link: "https://github.com/Chaiudbbhd/chat_application.git",
+    },
+    {
+      img: "https://i.ibb.co/JjktJN4s/bookstore.webp",
+      description: "An online platform that allows users to browse, purchase, and manage books digitally. It provides a seamless shopping experience with features like search, reviews, and secure payments",
+      link: "https://github.com/Chaiudbbhd/AWS-deployed-Book-Store.git",
+    },
+    {
+        img: "https://i.ibb.co/275Dmdps/whether.webp",
+        description: "A real-time system that predicts atmospheric conditions using data analysis and meteorological models. It provides accurate weather updates, alerts, and insights for better planning and decision-making.",
+        link: "https://github.com/Chaiudbbhd/Whether-Forecast-Responsive.git",
+      },
+  ];
+  
+
+const contactConfig = {
+    YOUR_EMAIL: "chaituchaitinya2005@gmail.com",
+    YOUR_FONE: "8328007804",
+    description: " am always eager to learn and adapt, approaching challenges with enthusiasm and determination. I strive to improve my skills and build a strong foundation in both technical and problem-solving areas, ensuring steady growth in my journey. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_nnvdytb",
+    YOUR_TEMPLATE_ID: "template_615pf9q",
+    YOUR_USER_ID: "trro9222gzdFyL57c",
+};
+
+const socialprofils = {
+    github: "https://github.com/Chaiudbbhd",
+    facebook: "https://www.facebook.com/chaitu.chaitinya.3",
+    linkedin: "https://www.linkedin.com/in/prasanna-kumar-g-3377a825a/",
+    instagram: "https://www.instagram.com/_lpk_in.4k?igsh=ZmFrb3ZwOTB5bGl2",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    education,
+    technicalSkills,
+    skillCategories,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
